124e6a06b9acf759b54a46c046f292ae64e4c0f5
AradM <arad.mohammadi99@gmail.com>
2021-03-08 10:20:08 +0330

    Add copmute_add function with test

--- calculator.py
+++ calculator.py
@@ -11,3 +11,17 @@ class Calculator:
             The name of this instance of the calculator.
         """
         self.name = name
+
+    def compute_add(self, op1, op2):
+        """
+        The constructor method for the calculator class.
+
+        :param op1: float
+            The first operand
+        :param op2: float
+            The second operand
+
+        :return : float
+            return the sum of two operands
+        """
+        return op1 + op2
